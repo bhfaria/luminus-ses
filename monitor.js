3c927a1473250ab2bd91c2f20e9747b666b10e80
Bruno Faria <bhfaria@gmail.com>
2023-07-16 21:11:51 -0300

    Implementação do OPTIN/OUT

--- monitor.js
+++ monitor.js
@@ -13,8 +13,7 @@ var ddb = new DynamoDB({});
 
 const crypto = require('crypto');
 
-async function persistMail(messageId,  send_timestamp, from, to, subject, status, status_timestamp) {
-  console.log(ddb);
+async function persistMail(messageId, send_timestamp, from, to, subject, type, subtype, status_timestamp) {
   //Atualiza/Insere item atual
   await ddb.putItem({
     TableName: 'ses_mail',
@@ -22,7 +21,7 @@ async function persistMail(messageId,  send_timestamp, from, to, subject, status
       "MessageId": {
         "S": messageId
       },
-      "send_timestamp" : {
+      "send_timestamp": {
         "S": send_timestamp
       },
       "from": {
@@ -34,14 +33,17 @@ async function persistMail(messageId,  send_timestamp, from, to, subject, status
       "subject": {
         "S": subject
       },
-      "status" : {
-        "S": status
+      "type": {
+        "S": type
+      },
+      "subtype": {
+        "S": subtype
       },
       "status_timestamp": {
         "S": status_timestamp
       }
     }
-    
+
   })/*.promise()*/;
 
   //Insere LOG
@@ -54,8 +56,11 @@ async function persistMail(messageId,  send_timestamp, from, to, subject, status
       "MessageId": {
         "S": messageId
       },
-      "status": {
-        "S": status
+      "type": {
+        "S": type
+      },
+      "subtype": {
+        "S": subtype
       },
       "status_timestamp": {
         "S": status_timestamp
@@ -65,71 +70,82 @@ async function persistMail(messageId,  send_timestamp, from, to, subject, status
 }
 
 module.exports.handler = async (event) => {
-  //console.log(JSON.stringify(event));
+  if (typeof event.Records !== "undefined") {
 
-  try {
-    await Promise.all(event.Records.map(async (record) => {
-      //let body = record;
-      //console.log(record);
+    try {
+      await Promise.all(event.Records.map(async (record) => {
+        if (typeof record.EventSource !== "undefined") {
+          if (record.EventSource == "aws:sns") {
+            let message = JSON.parse(record.Sns.Message);
 
-      //if (typeof body !== "undefined") {
-      if (typeof record.EventSource !== "undefined") {
-        if (record.EventSource == "aws:sns") {
-          let message = JSON.parse(record.Sns.Message);
+            if (typeof message !== "undefined") {
 
-          if (typeof message !== "undefined") {
+              let eventTimeStamp = undefined;
+              let subtype = "";
 
-            let eventTimeStamp = "";
-            
-            switch (message.eventType) {
-              case "Bounce": eventTimeStamp = message.bounce.timestamp; break;
-              case "Complaint": eventTimeStamp = message.complaint.timestamp; break;
-              case "Delivery": eventTimeStamp = message.delivery.timestamp; break;
-              case "Send": eventTimeStamp = message.mail.timestamp; break;
-              case "Reject": eventTimeStamp = message.reject.timestamp; break;
-              case "Open": eventTimeStamp = message.open.timestamp; break;
-              case "Click": eventTimeStamp = message.click.timestamp; break;
-              case "Rendering Failure": eventTimeStamp = message.failure.timestamp; break;
-              case "DeliveryDelay": eventTimeStamp = message.deliveryDelay.timestamp; break;
-              case "Subscription": eventTimeStamp = message.subscription.timestamp; break;
-            }
+              switch (message.eventType) {
+                case "Bounce": eventTimeStamp = message.bounce.timestamp; subtype = message.bounce.bounceType + " - " + message.bounce.bounceSubType; break;
+                case "Complaint": eventTimeStamp = message.complaint.timestamp; break;
+                case "Delivery": eventTimeStamp = message.delivery.timestamp; break;
+                case "Send": eventTimeStamp = message.mail.timestamp; break;
+                case "Reject": eventTimeStamp = message.reject.timestamp; break;
+                case "Open": eventTimeStamp = message.open.timestamp; break;
+                case "Click": eventTimeStamp = message.click.timestamp; break;
+                case "Rendering Failure": eventTimeStamp = message.failure.timestamp; break;
+                case "DeliveryDelay": eventTimeStamp = message.deliveryDelay.timestamp; break;
+                case "Subscription": eventTimeStamp = message.subscription.timestamp; break;
+              }
 
-            await persistMail(message.mail.messageId, message.mail.timestamp, message.mail.commonHeaders.from[0], message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.eventType, eventTimeStamp);
+              if (message.eventType == "Bounce") {
+                if (message.bounce.bounceType == "Permanent") {
+                  //Add to OnAccountSuppressionList - Automático
+                }
+                else if (message.bounce.bounceType == "Transient") {
 
-            if (message.eventType == "Send") {
-              //console.log("SEND To: %s. Subject: %s. Timestamp: %s. MessageID: %s", message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.mail.timestamp, message.mail.messageId);
-              //await persist(message.mail.messageId, message.mail.timestamp, message.mail.commonHeaders.from[0], message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.eventType, message.mail.timestamp);
 
-            }
-            else if (message.eventType == "Open") {
-              //console.log("OPEN To: %s. Subject: %s. Timestamp: %s. MessageID: %s", message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.mail.timestamp, message.mail.messageId);
-              //await persist(message.mail.messageId, message.mail.timestamp, message.mail.commonHeaders.from[0], message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.eventType, message.open.timestamp);
-            }
-            else if (message.eventType == "Bounce") {
-//              console.log("BOUNCE To: %s. Subject: %s. Timestamp: %s. MessageID: %s", message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.mail.timestamp, message.mail.messageId);
-            }
-            else if (message.eventType == "Complaint") {
-//              console.log("COMPLAINT To: %s. Subject: %s. Timestamp: %s. MessageID: %s", message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.mail.timestamp, message.mail.messageId);
+                }
+              } else if (message.eventType == "Send") {
+                //console.log("SEND To: %s. Subject: %s. Timestamp: %s. MessageID: %s", message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.mail.timestamp, message.mail.messageId);
+                //await persist(message.mail.messageId, message.mail.timestamp, message.mail.commonHeaders.from[0], message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.eventType, message.mail.timestamp);
 
-            }
-            else if (message.eventType == "Delivery") {
-//              console.log("DELIVERY To: %s. Subject: %s. Timestamp: %s. MessageID: %s", message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.mail.timestamp, message.mail.messageId);
+              }
+              else if (message.eventType == "Open") {
+                //console.log("OPEN To: %s. Subject: %s. Timestamp: %s. MessageID: %s", message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.mail.timestamp, message.mail.messageId);
+                //await persist(message.mail.messageId, message.mail.timestamp, message.mail.commonHeaders.from[0], message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.eventType, message.open.timestamp);
+              }
+              else if (message.eventType == "Complaint") {
+                //              console.log("COMPLAINT To: %s. Subject: %s. Timestamp: %s. MessageID: %s", message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.mail.timestamp, message.mail.messageId);
+
+              }
+              else if (message.eventType == "Delivery") {
+                //              console.log("DELIVERY To: %s. Subject: %s. Timestamp: %s. MessageID: %s", message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.mail.timestamp, message.mail.messageId);
+              }
+              else if (message.eventType == "Subscription") {
+                subtype = JSON.stringify(message.subscription.newTopicPreferences.topicSubscriptionStatus);
+/*                if (message.subscription.newTopicPreferences.unsubscribeAll == true) {
+                  subtype = "OPT_OUT";
+                }
+                else  {
+                  subtype = "OPT_IN";
+                }*/
+              }
+              else {
+                console.error("Message type desconhecido. %s", JSON.stringify(event));
+              }
+
+              await persistMail(message.mail.messageId, message.mail.timestamp, message.mail.commonHeaders.from[0], message.mail.commonHeaders.to[0], message.mail.commonHeaders.subject, message.eventType, subtype, eventTimeStamp);
             }
             else {
-              console.error("Message type desconhecido. %s", JSON.stringify(event));
+              console.error("Objeto message inválido. %s", JSON.stringify(record));
             }
-          }
-          else {
-            console.error("Objeto message inválido. %s", JSON.stringify(record));
 
           }
         }
-      }
-      //}
-    }));
-  }
-  catch (err) {
-    console.error(err);
+      }));
+    }
+    catch (err) {
+      console.error(err);
+    }
   }
 
   return {};
